7661b61f864986c00250480a1b30c567ec7102ed
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-03-17 09:56:42 -0500

    Added user prompt questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,23 @@
 // Assignment code here
 
 
+const generatePassword = () =>{
+  const passLength = prompt('Please enter a password between at least 8 characters and at most 128 characters. (y/n)')
+  const passIncludeLowercase = prompt('Would you like to include lowercase characters in your password? (y/n)')
+  const passIncludeUppercase = prompt('Would you like to include uppercase characters in your password? (y/n)')
+  const passIncludeNumbers = prompt('Would you like to include numbers in your password? (y/n)')
+  const passIncludeSpecialChar = prompt('Would you like to include special characters in your password? (y/n)')
+
+  
+}
+
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
